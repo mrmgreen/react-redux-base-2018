6d27c2fdad1ea381c2f1c9ce50039f155521b673
Green <mgr52@ostml0253758.bskyb.com>
2018-07-23 13:38:37 +0100

    redux setup

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import aReducer from '../reducers';
+
+const store = createStore(aReducer);
 
 ReactDOM.render(
-  <div>Hello you!</div>,
+  <Provider store={store}>
+    <div>Hello you!s</div>
+  </Provider>,
   document.getElementById('root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
